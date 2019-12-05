d7ee4789809e2186e436426708c5dda7f13874e0
SangharshSeth <sethsangharsh@outlook.com>
2019-12-05 23:25:04 +0530

    Fixed anagram

--- LCAnagram.cpp
+++ LCAnagram.cpp
@@ -1,19 +1,22 @@
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-   map<string,vector<string>>hashtable;
-   string temp;
-    vector<vector<string>>Result;
-    for(int i = 0;i<strs.size();i++){
-        temp = srts[i];
-        sort(strs[i].begin(),strs[i].end());
-        hashtable[strs[i]].push_back(temp);
-    }
-   for(auto i:hashtable)
-       Result.push_back(i.second);
-        
-        
-        
- return Result;
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        map<string, vector<string>> hashtable;
+        string temp;
+        vector<vector<string>> Result;
+        for (int i = 0; i < strs.size(); i++)
+        {
+            temp = strs[i];
+            sort(strs[i].begin(), strs[i].end());
+            hashtable[strs[i]].push_back(temp);
+        }
+        for (auto i : hashtable)
+            Result.push_back(i.second);
+
+        return Result;
     }
 };
