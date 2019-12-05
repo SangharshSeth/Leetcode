7aa14826fa466c352f32e3b6a01612a607286bc6
SangharshSeth <sethsangharsh@outlook.com>
2019-12-05 20:05:00 +0530

    progress updated

---
+++ twosum.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> twosum(vector<int> &v, int target)
+{
+    target = target - 30;
+    int maximum = INT32_MIN;
+    map<int, int> map;
+    vector<int> result;
+    int complement;
+    for (int i = 0; i < v.size(); i++)
+    {
+        complement = target - v[i];
+        auto it = map.find(complement);
+        if (it != map.end())
+        {
+            if (max(v[i], complement) > maximum)
+            {
+                maximum = max(v[i], complement);
+                result = {i, map[complement]};
+            }
+        }
+        map[v[i]] = i;
+    }
+    return result;
+}
+int main()
+{
+    //two sum
+    vector<int> v = {20, 50, 40, 25, 30, 10};
+    vector<int> result = twosum(v, 90);
+    for (auto i : result)
+        cout << i << " ";
+
+    return 0;
+}
