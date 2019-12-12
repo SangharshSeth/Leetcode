d12ad4e6151f1d8d8342916214350358f958c266
SangharshSeth <sethsangharsh@outlook.com>
2019-12-12 20:24:39 +0530

    i Dunno whts happening

---
+++ Pointers_hell/cmake-build-debug/CMakeFiles/Pointers_hell.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pointers_hell.dir/main.cpp.o"
+  "Pointers_hell.exe"
+  "Pointers_hell.pdb"
+  "libPointers_hell.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pointers_hell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Pointers_hell/cmake-build-debug/CMakeFiles/Pointers_hell.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Pointers_hell.
+# This may be replaced when dependencies are built.
